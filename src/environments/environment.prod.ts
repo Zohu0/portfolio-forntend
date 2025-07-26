65cd9424f9014c947c0dd9d87ac13a2c578b95b8
Zohu0 <shaikhzohaib477@gmail.com>
2025-07-26 21:56:19 +0530

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,13 @@
 export const environment = {
   production: true,
 
-  apiUrlUser: process.env['NG_APP_API_URL_USER'] || 'https://portfolio-backend-production-e1b6.up.railway.app/usercontroller',
-  apiUrlEducation: process.env['NG_APP_API_URL_EDUCATION'] || 'https://portfolio-backend-production-e1b6.up.railway.app/educationcontroller',
-  apiUrlExperience: process.env['NG_APP_API_URL_EXPERIENCE'] || 'https://portfolio-backend-production-e1b6.up.railway.app/experiencecontroller',
-  apiUrlProject: process.env['NG_APP_API_URL_PROJECT'] || 'https://portfolio-backend-production-e1b6.up.railway.app/projectcontroller',
-  apiUrlSkill: process.env['NG_APP_API_URL_SKILL'] || 'https://portfolio-backend-production-e1b6.up.railway.app/skillcontroller',
+  apiUrlUser: 'https://portfolio-backend-production-e1b6.up.railway.app/usercontroller',
+
+  apiUrlEducation: 'https://portfolio-backend-production-e1b6.up.railway.app/educationcontroller',
+
+  apiUrlExperience: 'https://portfolio-backend-production-e1b6.up.railway.app/experiencecontroller',
+
+  apiUrlProject: 'https://portfolio-backend-production-e1b6.up.railway.app/projectcontroller',
+
+  apiUrlSkill: 'https://portfolio-backend-production-e1b6.up.railway.app/skillcontroller',
 };
