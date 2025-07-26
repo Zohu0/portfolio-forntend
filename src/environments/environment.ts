9e76d663d06d472180283617f413255947e6dd0b
Zohu0 <shaikhzohaib477@gmail.com>
2025-07-27 00:03:56 +0530

    changes dev to prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,26 @@
 export const environment = {
   production: false,
 
-  apiUrlUser: 'http://localhost:8080/usercontroller',
+//   apiUrlUser: 'http://localhost:8080/usercontroller',
 
-  apiUrlEducation: 'http://localhost:8080/educationcontroller',
+//   apiUrlEducation: 'http://localhost:8080/educationcontroller',
 
-  apiUrlExperience: 'http://localhost:8080/experiencecontroller',
+//   apiUrlExperience: 'http://localhost:8080/experiencecontroller',
 
-  apiUrlProject: 'http://localhost:8080/projectcontroller',
+//   apiUrlProject: 'http://localhost:8080/projectcontroller',
 
-  apiUrlSkill: 'http://localhost:8080/skillcontroller',
+//   apiUrlSkill: 'http://localhost:8080/skillcontroller',
+
+
+  apiUrlUser: 'https://portfolio-backend-production-e1b6.up.railway.app/usercontroller',
+
+  apiUrlEducation: 'https://portfolio-backend-production-e1b6.up.railway.app/educationcontroller',
+
+  apiUrlExperience: 'https://portfolio-backend-production-e1b6.up.railway.app/experiencecontroller',
+
+  apiUrlProject: 'https://portfolio-backend-production-e1b6.up.railway.app/projectcontroller',
+
+  apiUrlSkill: 'https://portfolio-backend-production-e1b6.up.railway.app/skillcontroller',
 
   
 };
